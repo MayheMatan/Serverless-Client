13f08168228e1ac656360307e5a5de9bcffc117f
myusername <Matanfriedleet@hotmail.com>
2020-10-26 17:43:50 +0200

    added login/signup functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Login from './views/Login';
+import Users from './views/Users'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 900,
+    textAlign: 'center',
+    margin: '0 auto',
+    marginTop: '50px'
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={classes.root}>
+        <Typography variant="h3" component="h3" gutterBottom>
+          Welcome to Amazing Serverless API!
+        </Typography>
+        <Typography variant="h5" component="h5" gutterBottom>
+          This is a simple demonstration of a serverless app, using Lambda functions
+        </Typography>
+      </div>
+      <Router>
+        <Route exact path="/" render={() => <Login />}/>
+        <Route exact path="/users" render={() => <Users />}/>
+      </Router>
+    </>
   );
 }
 
